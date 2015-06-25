0e0776e972143c1be59e190c8975ee0c2b9665b9
Baulieu <b.de.reboul@gmail.com>
2015-06-25 12:31:47 +0200

    generalisation a tous les parametres de la v1

--- v1.py
+++ v1.py
@@ -72,50 +72,53 @@ while i < 110:
     t = tools.import_result()
     data.appendResult(name, t)
     a[name] = Analysis(name, settings)
-    a[name].addResult(t)
     a[name].add_frames(tools.import_perf())
-    subprocess.call(['cp', 'cam3.avi', 'results/'+name+'.avi'])
+    a[name].addResult(t)
+    # subprocess.call(['cp', 'cam3.avi', 'results/'+name+'.avi'])
     i += 20
-""" --- points_min --- + --- 3 to 15 --- ""
+""" --- points_min --- + --- 3 to 15 --- """
 i = 3
 while i < 15:
     stri = "points_min"
     name = stri + str(i)
     settings.change(stri, i)
     # subprocess.call(['./exe'])
-    t = tools.openBackup()
+    t = tools.import_result()
     data.appendResult(name, t)
     a[name] = Analysis(name, settings)
+    a[name].add_frames(tools.import_perf())
     a[name].addResult(t)
-    subprocess.call(['cp', 'cam3.avi', 'results/'+name+'.avi'])
+    # subprocess.call(['cp', 'cam3.avi', 'results/'+name+'.avi'])
     i += 3
-"" --- surface_min --- + --- 5 to 50 --- ""
+""" --- surface_min --- + --- 5 to 50 --- """
 i = 5
 while i < 55:
     stri = "surface_min"
     name = stri + str(i)
     settings.change(stri, i)
     # subprocess.call(['./exe'])
-    t = tools.openBackup()
+    t = tools.import_result()
     data.appendResult(name, t)
     a[name] = Analysis(name, settings)
+    a[name].add_frames(tools.import_perf())
     a[name].addResult(t)
-    subprocess.call(['cp', 'cam3.avi', 'results/'+name+'.avi'])
+    # subprocess.call(['cp', 'cam3.avi', 'results/'+name+'.avi'])
     i += 10
-"" --- between_target --- ++ --- 100 to 1000 --- ""
+""" --- between_target --- ++ --- 100 to 1000 --- """
 i = 100
 while i < 1000:
     stri = "between_target"
     name = stri + str(i)
     settings.change(stri, i)
     # subprocess.call(['./exe'])
-    t = tools.openBackup()
+    t = tools.import_result()
     data.appendResult(name, t)
     a[name] = Analysis(name, settings)
+    a[name].add_frames(tools.import_perf())
     a[name].addResult(t)
-    subprocess.call(['cp', 'cam3.avi', 'results/'+name+'.avi'])
+    # subprocess.call(['cp', 'cam3.avi', 'results/'+name+'.avi'])
     i += 100
-"" --- distance_3d and distance_pixel --- ++ --- 0.5 to 5 and 20 to 200 --- ""
+""" --- distance_3d and distance_pixel --- ++ --- 0.5 to 5 and 20 to 200 --- """
 i = 0.5
 while i < 5:
     stri = "distance_3d"
@@ -123,25 +126,27 @@ while i < 5:
     settings.change(stri, i)
     settings.change("distance_pixel", i/40)
     # subprocess.call(['./exe'])
-    t = tools.openBackup()
+    t = tools.import_result()
     data.appendResult(name, t)
     a[name] = Analysis(name, settings)
+    a[name].add_frames(tools.import_perf())
     a[name].addResult(t)
-    subprocess.call(['cp', 'cam3.avi', 'results/'+name+'.avi'])
+    # subprocess.call(['cp', 'cam3.avi', 'results/'+name+'.avi'])
     i += 0.5
-"" --- size_min --- ++ --- 2 to 50 --- ""
+""" --- size_min --- ++ --- 2 to 50 --- """
 i = 2
 while i < 52:
     stri = "size_min"
     name = stri + str(i)
     settings.change(stri, i)
     # subprocess.call(['./exe'])
-    t = tools.openBackup()
+    t = tools.import_result()
     data.appendResult(name, t)
     a[name] = Analysis(name, settings)
+    a[name].add_frames(tools.import_perf())
     a[name].addResult(t)
-    subprocess.call(['cp', 'cam3.avi', 'results/'+name+'.avi'])
-    i += 5"""
+    # subprocess.call(['cp', 'cam3.avi', 'results/'+name+'.avi'])
+    i += 5
 
 """ et maintenant, petite seance de visionnage de CAM3 des familles."""
 
@@ -156,60 +161,60 @@ while i < 110:
     xls.add_analysis(a[name], name, i)
     i += 20
 
-"""i = 3
+i = 3
 while i < 15:
     name = "points_min" + str(i)
-    subprocess.call(['vlc', 'results/'+name+'.avi', 'vlc://quit'])  # reading CAM3 and closing it at the end
+    # subprocess.call(['vlc', 'results/'+name+'.avi', 'vlc://quit'])  # reading CAM3 and closing it at the end
     temp = input("nombre de faux positifs :    **attention : ne taper que des chiffres! ou alors exit pour arreter le programme***\n")  # reading the number
     if temp == "exit":
         raise NameError('arret volontaire du processus. For the watch.')
     a[name].addFalsep(temp)  # sending it to the analysis module
-    xls.add_analysis(a[name])
+    xls.add_analysis(a[name], name, i)
     i += 3
 
 i = 5
 while i < 55:
     name = "surface_min" + str(i)
-    subprocess.call(['vlc', 'results/'+name+'.avi', 'vlc://quit'])  # reading CAM3 and closing it at the end
+    # subprocess.call(['vlc', 'results/'+name+'.avi', 'vlc://quit'])  # reading CAM3 and closing it at the end
     temp = input("nombre de faux positifs :    **attention : ne taper que des chiffres! ou alors exit pour arreter le programme***\n")  # reading the number
     if temp == "exit":
         raise NameError('arret volontaire du processust. For the watch.')
     a[name].addFalsep(temp)  # sending it to the analysis module
-    xls.add_analysis(a[name])
+    xls.add_analysis(a[name], name, i)
     i += 10
 
 i = 100
 while i < 1000:
     name = "between_target" + str(i)
-    subprocess.call(['vlc', 'results/'+name+'.avi', 'vlc://quit'])  # reading CAM3 and closing it at the end
+    # subprocess.call(['vlc', 'results/'+name+'.avi', 'vlc://quit'])  # reading CAM3 and closing it at the end
     temp = input("nombre de faux positifs :    **attention : ne taper que des chiffres! ou alors exit pour arreter le programme***\n")  # reading the number
     if temp == "exit":
         raise NameError('arret volontaire du processus. For the watch.')
     a[name].addFalsep(temp)  # sending it to the analysis module
-    xls.add_analysis(a[name])
+    xls.add_analysis(a[name], name, i)
     i += 100
 
 i = 0.5
 while i < 5:
     name = "distance_3d" + str(i)
-    subprocess.call(['vlc', 'results/'+name+'.avi', 'vlc://quit'])  # reading CAM3 and closing it at the end
+    # subprocess.call(['vlc', 'results/'+name+'.avi', 'vlc://quit'])  # reading CAM3 and closing it at the end
     temp = input("nombre de faux positifs :    **attention : ne taper que des chiffres! ou alors exit pour arreter le programme***\n")  # reading the number
     if temp == "exit":
         raise NameError('arret volontaire du processus. For the watch.')
     a[name].addFalsep(temp)  # sending it to the analysis module
-    xls.add_analysis(a[name])
+    xls.add_analysis(a[name], name, i)
     i += 0.5
 
 i = 2
 while i < 52:
     name = "size_min" + str(i)
-    subprocess.call(['vlc', 'results/'+name+'.avi', 'vlc://quit'])  # reading CAM3 and closing it at the end
+    # subprocess.call(['vlc', 'results/'+name+'.avi', 'vlc://quit'])  # reading CAM3 and closing it at the end
     temp = input("nombre de faux positifs :    **attention : ne taper que des chiffres! ou alors exit pour arreter le programme***\n")  # reading the number
     if temp == "exit":
         raise NameError('arret volontaire du processus. For the watch.')
     a[name].addFalsep(temp)  # sending it to the analysis module
-    xls.add_analysis(a[name])
-    i += 5"""
+    xls.add_analysis(a[name], name, i)
+    i += 5
 
 xls.write()
 
